822585a0aea3dc6eb8df491919ab1affe54d0da6
Maria <86696680+sweetreprise@users.noreply.github.com>
2021-07-27 19:42:26 -0400

    Add files via upload

---
+++ let-and-const.js
@@ -0,0 +1,25 @@
+//ES5
+
+var PI = 3.14;
+PI = 42;
+
+//ES2015
+
+const PI = 3.14;
+
+
+// What is the difference between var and let?
+
+// var is function scoped and hoisted to the top of the script file. let is block scoped and not hoisted. you are also not able to redeclare let; you can do so with var.
+
+// What is the difference between var and const?
+
+//var is function scoped and hoisted. you are also able to redeclare var. const is block scoped and is not able to be redeclared nor reassigned.
+
+// What is the difference between let and const?
+
+// let is able to be reassigned. const is not.
+
+// What is hoisting?
+
+//hoisting refers to the way javascript moves declarations to the top of the scope, so they are accessible before execution
